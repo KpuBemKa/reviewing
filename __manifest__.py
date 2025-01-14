32b5a58d49f49afd86da934493710f1beef38487
KpuBemKa <kakoitopis@gmail.com>
2025-01-14 14:01:12 +0000

    renamed files

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Reviewing",
     "version": "0.1",
-    "depends": ["base", "web"],
+    "depends": ["base", "web", "crm"],
     "author": "MARS dev",
     "category": "Category",
     "application": True,
@@ -14,14 +14,14 @@
     "assets": {
         "web.assets_backend": [
             "reviewing/static/src/js/audio_field.js",
-            "reviewing/static/src/js/expandable_text.js",
+            "reviewing/static/src/js/expandable_text_field.js",
             # ---
             "reviewing/static/src/scss/audio_field.scss",
-            "reviewing/static/src/scss/expandable_text.scss",
+            "reviewing/static/src/scss/expandable_text_field.scss",
             "reviewing/static/src/scss/style.scss",
             # ---
             "reviewing/static/src/xml/audio_field.xml",
-            "reviewing/static/src/xml/expandable_text.xml",
+            "reviewing/static/src/xml/expandable_text_field.xml",
         ],
     },
     "controllers": ["controllers.main_controller.MainController"],
